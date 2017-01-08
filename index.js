67c433ab44d7458ec302a0fde1760f040ca441a1
Johnry Mangonon <johnry.mangonon@bbc.co.uk>
2017-01-08 18:50:38 +0000

    add different file logic

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var static = require('node-static');
 var localeArray = ["UK", "GNL", "MUNDO", "RUSSIAN", "HINDI", "CYMRU", "ARABIC"];
+var fileArray = ["handrail_list", "followtopics", "searchtopics", ""]
 var file = new static.Server('./public');
 require('http').createServer(function(request, response) {
   request.addListener('end', function() {
     file.serve(request, response, function(err, res) {
         var requestUrl = request.url;
-        var whitelist = "handrail_list";
         if (requestUrl.includes(whitelist)) {
-            var newUrl = "/static/" + getLocale(requestUrl) + "/default/handrail_list.json";
+            var newUrl = "/static/" + getLocale(requestUrl) + "/default/" + getFileFromUrl(requestUrl) + ".json";
             file.serveFile(newUrl, 200, {}, request, response);
         }
     });
@@ -18,6 +18,14 @@ function getLocale(url) {
     for (var i = 0; i < localeArray.length; i++) {
         if (url.includes(localeArray[i])) {
             return localeArray[i];
+        }
+    }
+}
+
+fuction getFileFromUrl(url) {
+    for (var i = 0; i < fileArray.length; i++) {
+        if (url.indexOf(substrings[length])!=-1) {
+            return fileArray[i]
         }
     }
 }
