23d51a50cd7a243d6b36cb2370b5852c80832139
Johnry Mangonon <johnry.mangonon@bbc.co.uk>
2017-01-08 17:38:59 +0000

    fix servefile call

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('http').createServer(function(request, response) {
   request.addListener('end', function() {
     file.serve(request, response, function(err, res) {
         if (request.url.includes("handrail_list")) {
-            file.serveFile('/static/UK/default/handrail_list.json')
+            file.serveFile('/static/UK/default/handrail_list.json', 200, {}, request, response);
         }
     });
   }).resume();
