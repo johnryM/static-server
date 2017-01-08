f9d22ce1ce09718def1cc8d376534f362537b258
Johnry Mangonon <johnry.mangonon@bbc.co.uk>
2017-01-08 19:20:07 +0000

    fix typos

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var static = require('node-static');
 var localeArray = ["UK", "GNL", "MUNDO", "RUSSIAN", "HINDI", "CYMRU", "ARABIC"];
-var fileArray = ["handrail_list", "followtopics", "searchtopics", ""]
+var fileArray = ["handrail_list", "handrail", "followtopics", "searchtopics"];
 var file = new static.Server('./public');
 require('http').createServer(function(request, response) {
   request.addListener('end', function() {
