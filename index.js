d3b1e158eaa494bf2c8801161078751438d784c0
Johnry Mangonon <johnry.mangonon@bbc.co.uk>
2017-01-08 18:39:35 +0000

    fix url logic

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var static = require('node-static');
+var localeArray = ["UK", "GNL", "MUNDO", "RUSSIAN", "HINDI", "CYMRU", "ARABIC"];
 var file = new static.Server('./public');
 require('http').createServer(function(request, response) {
   request.addListener('end', function() {
@@ -6,9 +7,17 @@ require('http').createServer(function(request, response) {
         var requestUrl = request.url;
         var whitelist = "handrail_list";
         if (requestUrl.includes(whitelist)) {
-            var newUrl = requestUrl.slice(0, requestUrl.indexOf(whitelist)) + "handrail_list.json";
+            var newUrl = "/static/" + getLocale(requestUrl) + "/default/handrail_list.json";
             file.serveFile(newUrl, 200, {}, request, response);
         }
     });
   }).resume();
 }).listen(process.env.PORT || 3000);
+
+function getLocale(url) {
+    for (var i = 0; i < localeArray.length; i++) {
+        if (url.includes(localeArray[i])) {
+            return localeArray[i];
+        }
+    }
+}
