8ed49a52f164b18aa503f11d86107e62923bbd91
Johnry Mangonon <johnry.mangonon@bbc.co.uk>
2017-01-08 17:35:15 +0000

    test servefile

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-//just a check
 var static = require('node-static');
 var file = new static.Server('./public');
 require('http').createServer(function(request, response) {
   request.addListener('end', function() {
-    file.serve(request, response);
+    file.serve(request, response, function(err, res) {
+        if (request.url.includes("handrail_list")) {
+            file.serveFile('/static/UK/default/handrail_list.json')
+        }
+    });
   }).resume();
 }).listen(process.env.PORT || 3000);
