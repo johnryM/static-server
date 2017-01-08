56643de2cbf602772fdb5ca769ed03e2cbadcf73
Johnry Mangonon <johnry.mangonon@bbc.co.uk>
2017-01-08 18:07:38 +0000

    serve appropriate handrail based on locale

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var file = new static.Server('./public');
 require('http').createServer(function(request, response) {
   request.addListener('end', function() {
     file.serve(request, response, function(err, res) {
-        if (request.url.includes("handrail_list")) {
-            file.serveFile('/static/UK/default/handrail_list.json', 200, {}, request, response);
+        var requestUrl = request.url;
+        var whitelist = "handrail_list";
+        if (requestUrl.includes(whitelist)) {
+            var newUrl = requestUrl.slice(0, requestUrl.indexOf(whitelist));
+            file.serveFile(newUrl + 'handrail_list.json', 200, {}, request, response);
         }
     });
   }).resume();
